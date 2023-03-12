400af19d597eae53acda1fe9163d917dba1c981a
Lemolin <100899112+Lemolin@users.noreply.github.com>
2023-03-13 01:50:19 +1030

    Update Human.cpp

--- Human.cpp
+++ Human.cpp
@@ -6,32 +6,32 @@
 Human::Human(string name):m_name(name){
 }
 Move* Human::makeMove() {
-	string A[] = { "Zombie","Monkey","Ninjia","Pirate","Robot","Paper","Rock","Scissors" };
+	//string A[] = { "Zombie","Monkey","Ninjia","Pirate","Robot","Paper","Rock","Scissors" };
 	string m_move;
 
-	while (1) {
+	//while (1) {
 		cout << "Enter a Move:";
 		cin >> m_move;
 
-		bool isIn = false;
+		//bool isIn = false;
 
-		for (int i = 0; i < (sizeof(A) / sizeof(A[0])); i++) {
-			if (m_move == A[i]) {
-				isIn = true;
-			}
-		}
+		//for (int i = 0; i < (sizeof(A) / sizeof(A[0])); i++) {
+			//if (m_move == A[i]) {
+				//isIn = true;
+			//}
+		//}
 
-		if (isIn) {
+		//if (isIn) {
 			Move* move = new Move(m_move);
 			return move;
-			break;
-		}
+			//break;
+		//}
 
-		else {
-			cout << "Please enter the correct move" << endl;
-			continue;
-		}
-	}
+		//else {
+			//cout << "Please enter the correct move" << endl;
+			//continue;
+		//}
+	//}
 }
 string Human::getName() {
 	return m_name;
